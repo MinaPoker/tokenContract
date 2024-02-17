d1f68ad4b8d5b91ba20661a704e250e6e2632d51
0x <101751723+0xKryptonian@users.noreply.github.com>
2024-02-17 21:16:37 +0530

    created basic MPC contract files

---
+++ src/Token.ts
@@ -0,0 +1,13 @@
+import {
+    SmartContract,
+    state,
+    State,
+    method,
+    DeployArgs,
+    Permissions,
+    UInt64,
+    PublicKey,
+    Signature,
+} from 'o1js';
+
+const tokenSymbol = 'MPC'; // Mina Poker Coin
