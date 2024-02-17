b6737edd89c778772262cd7b9669b0b71f4d855d
0x <101751723+0xKryptonian@users.noreply.github.com>
2024-02-17 17:02:29 +0530

    created MPC contract frame

--- src/Token.ts
+++ src/Token.ts
@@ -11,3 +11,33 @@ import {
 } from 'o1js';
 
 const tokenSymbol = 'MPC'; // Mina Poker Coin
+
+export class MPCtoken extends SmartContract {
+
+    @state(UInt64) totalAmountInCirculation = State<UInt64>();
+
+    // deployer rules & permission interface setup
+    deploy (args: DeployArgs){
+        super.deploy(args);
+        const permissions = Permissions.proof();
+        this.account.permissions.set({
+            ...Permissions.default(),
+            editState: permissionToEdit,
+            setTokenSymbol:permissionToEdit,
+            send: permissionToSend,
+            receive: permissionToReceive,
+        });
+    }
+
+    @method init(){
+
+    }
+    
+    @method mint(){
+
+    }
+
+    @method sendTokens(){
+        
+    }
+}
