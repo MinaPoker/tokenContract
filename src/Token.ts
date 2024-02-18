6bd1c97acbd79666a808e850f548e5d10e77180a
0x <101751723+0xKryptonian@users.noreply.github.com>
2024-02-18 21:12:07 +0530

    created send token function

--- src/Token.ts
+++ src/Token.ts
@@ -17,38 +17,49 @@ export class MPCtoken extends SmartContract {
     @state(UInt64) totalAmountInCirculation = State<UInt64>();
 
     // deployer rules & permission interface setup
-    deploy (args: DeployArgs){
+    deploy(args: DeployArgs) {
         super.deploy(args);
         const permissions = Permissions.proof();
         this.account.permissions.set({
             ...Permissions.default(),
             editState: permissionToEdit,
-            setTokenSymbol:permissionToEdit,
+            setTokenSymbol: permissionToEdit,
             send: permissionToSend,
             receive: permissionToReceive,
         });
     }
 
-    @method init(){ // initialize the token & only called once
+    @method init() { // initialize the token & only called once
         super.init();
         this.account.tokenSymbol.set(tokenSymbol);
         this.totalAmountInCirculation.set(0);
     }
-    
+
     @method mint(
         recieverAddress: PublicKey,
         amount: UInt64,
         signature: Signature
-    ){
+    ) {
         let totalAmountInCirculation = this.totalAmountInCirculation.get();
         this.totalAmountInCirculation.assertEquals(totalAmountInCirculation);
 
         let newTotalAmountInCirculation = totalAmountInCirculation.add(amount);
         signature.verify(this.address, amount.toFields().concat(recieverAddress.toFields())).assertTrue();
-        this.token.mint({address: recieverAddress, amount})
+        this.token.mint({ address: recieverAddress, amount })
         this.totalAmountInCirculation.set(newTotalAmountInCirculation);
     }
 
-    @method sendTokens(){
-        
+    @method sendTokens(
+        senderAddress: PublicKey,
+        recieverAddress: PublicKey,
+        amount: UInt64,
+    ) {
+        this.token.send({
+            from: senderAddress,
+            to: recieverAddress,
+            amount
+        });
+    }
+
+
 }
